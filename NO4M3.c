b20d569f34b827b2025673882e0be2351ab58cec
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2017-10-24 20:35:52 +0700

    NO4M3.c
    First Commit

---
+++ NO4M3.c
@@ -0,0 +1,14 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void* thread_faktorial(void* parameter){
+
+int faktorial;
+int i;
+
+    for(i=faktorial;i<0;--i){
+        faktorial *= i;
+
+    }
+
+}

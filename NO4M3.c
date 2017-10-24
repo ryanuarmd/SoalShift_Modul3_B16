cb096e3a3f4223b7f2094e86242e1a091af3a197
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2017-10-25 01:06:16 +0700

    NO4M3
    Pembetulan fungsi faktorial
    Penambahan fungsi main

--- NO4M3.c
+++ NO4M3.c
@@ -2,14 +2,26 @@
 #include <stdio.h>
 
 void* thread_faktorial(void* parameter){
+int *x = (int*)parameter;
 
 int faktorial=1;
 int i;
 
-    for(i=(int*)parameter;i>0;--i){
+    for(i=*x;i>0;--i){
         faktorial *= i;
-    printf("%d", faktorial);
 }
+printf("Hasil Dari %d! = %d \n", *x, faktorial);
 
+}
+int main(){
+    pthread_t tid[100];
+    int input_num[100];
+    int c=0;
+    while(scanf("%d",&input_num[c])!=EOF)
+    {
+        pthread_create(&(tid[c]), NULL, thread_faktorial,&input_num[c]);
+        pthread_join(tid[c],NULL);
+        c++;
+    }
 }
     

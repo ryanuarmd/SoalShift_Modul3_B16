da911f468ab065210de04633d6b144cc3af01061
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2017-10-25 16:45:04 +0700

    NO5M3 penambahan main

--- soal5.c
+++ soal5.c
@@ -12,11 +12,24 @@ void* cari_nama(void* nama_idola){
     
     //mencari kata
     fscanf (novel,"%s",)
-    if(strcmp())
+    //if(strcmp())
     
     //output nama orang yang telah disebutkan
     printf("%s : %d ", nama, jumlah_nama);
     //tutup file
     fclose(novel);
     
+}
+int main(){
+    pthread_t tid[2];
+    string input_nama[10];
+    int c=0;
+
+    while(scanf("%s",input_nama[c])!=EOF){
+        pthread_create(&tid, NULL, cari_nama,&intput_nama);
+        pthread_join(tid[c],NULL);
+        c++;
+    }
+
+   
 }
